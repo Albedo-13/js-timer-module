d3505523d1d2e08af2acecc56a334949b45fcdc2
Pavel Prokopenya <prokopenya.work@gmail.com>
2022-08-17 14:05:00 +0300

    small code changes & package-lock.json added

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ window.addEventListener('DOMContentLoaded', () => {
     timer({
         endDate: new Date(1900, 1, 1),  // Put your end date here
         parentSelector: ".timer",
-        monthSelector: "#months",
         daySelector: "#days",
         hourSelector: "#hours",
         minuteSelector: "#minutes",
